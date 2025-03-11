cf6acd3aac098bcc713dd0978ef93a3a43bd31dd
meme sharing <nityamittal.coding@gmail.com>
2025-03-12 00:55:05 +0530

    done

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,39 +1,60 @@
-import { DarkTheme, DefaultTheme, ThemeProvider } from '@react-navigation/native';
-import { useFonts } from 'expo-font';
-import { Stack } from 'expo-router';
-import * as SplashScreen from 'expo-splash-screen';
-import { StatusBar } from 'expo-status-bar';
-import { useEffect } from 'react';
-import 'react-native-reanimated';
+//  import { Stack } from "expo-router";
 
-import { useColorScheme } from '@/hooks/useColorScheme';
 
-// Prevent the splash screen from auto-hiding before asset loading is complete.
-SplashScreen.preventAutoHideAsync();
+// import { useState } from "react";
+// import { CreateTripContext } from './../context/CreateTripContext';
 
-export default function RootLayout() {
-  const colorScheme = useColorScheme();
-  const [loaded] = useFonts({
-    SpaceMono: require('../assets/fonts/SpaceMono-Regular.ttf'),
-  });
+// export default function RootLayout() {
+//   const [tripData, setTripData] = useState([]);
 
-  useEffect(() => {
-    if (loaded) {
-      SplashScreen.hideAsync();
-    }
-  }, [loaded]);
+//   return (
+//     <CreateTripContext.Provider value={{ tripData, setTripData }}>
+//       <Stack screenOptions={{ headerShown: false
+        
+//        }}>
+//         <Stack.Screen name="(tabs)" />
+//       </Stack>
+//     </CreateTripContext.Provider>
+//   );
+// }
 
-  if (!loaded) {
-    return null;
-  }
+// // import { Stack } from "expo-router";
+// import { useFonts } from 'expo-font';
+// import {CreateTripContext} from '@/context/CreateTripContext'
+// import {useState} from "react";
 
+// export default function RootLayout() {
+//   const [tripData,setTripData] = useState([]);
+
+//   // useFonts({
+//   //   'Outfit': require('./../assets/fonts/Outfit-Regular.ttf'),
+//   //   'Outfit-Medium': require('./../assets/fonts/Outfit-Medium.ttf'),
+//   //   'Outfit-Bold': require('./../assets/fonts/Outfit-Bold.ttf'),
+//   // });
+//   return (
+//     // <CreateTripContext.Provider value={{tripData,setTripData}}>
+
+//     <Stack screenOptions={{headerShown:false}}>
+//       {/* <Stack.Screen name="index" options={{headerShown:false}}/> */}
+//       <Stack.Screen name="(tabs)"/>
+//     </Stack>
+//     // </CreateTripContext.Provider>
+//   );
+// }
+
+// import { Stack } from "expo-router";
+
+// export default function Layout() {
+//   return <Stack />;
+// }
+
+import {Stack} from "expo-router";
+
+export default function Layout() {
   return (
-    <ThemeProvider value={colorScheme === 'dark' ? DarkTheme : DefaultTheme}>
-      <Stack>
-        <Stack.Screen name="(tabs)" options={{ headerShown: false }} />
-        <Stack.Screen name="+not-found" />
-      </Stack>
-      <StatusBar style="auto" />
-    </ThemeProvider>
+    <Stack>
+      <Stack.Screen name="index" options={{ headerShown: false }} />
+    </Stack>
   );
 }
+
